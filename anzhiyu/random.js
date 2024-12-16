1a8176dc06a6cbd0d1bc404637306c5eddb6b005
badboy <badboy@yeah.net>
2024-12-16 16:43:44 +0800

    Site updated: 2024-12-16 16:43:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/16/这是一篇新的博文/","2024/12/16/hello-world/"];function toRandomPost(){
+var posts=["wenzhang/这是一篇新的博文/","wenzhang/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
